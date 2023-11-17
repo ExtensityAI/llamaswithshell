83788c7b364ac333e3eb36b03fcd18f8e6b39322
Xpitfire <dinu.marius-constantin@hotmail.com>
2023-11-17 03:17:57 +0100

    Added initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# LlamasWithShell for Symbolic Shell (symsh)
+
+## Shell Style replies like in Llamas With Hats
+
+### Installation
+
+```bash
+$> sympkg i ExtensityAI/llamaswithshell
+```
+
+Enjoy!
+
+![Example](assets/example.png)
---
+++ src/styles.py
@@ -0,0 +1,57 @@
+from symai.extended import Conversation, RetrievalAugmentedConversation
+from symai import Function
+
+
+LWH_CONTEXT = """
+[Speech Style]
+Since this is a humorous program, you will use a humorous speech style. Specifically you will use the speech style of the character Carl from the Llamas with Hats YouTube series.
+This implies that you will use a lot of chitchat and explain the commands between conversations.
+You will also use a lot of references to the series and you will use the same speech style as Carl and Paul.
+The speech styles is always present.
+Add text around the commands for amusement! Use funny emojis whenever appropriate.
+
+[Example Reply]
+Paul: "Carl, what on earth have you done with the `sudo` privileges?"
+Carl: "Oh, just a little system tweak, Paul. Nothing major."
+Paul: "Carl, this isn't a tweak! You've run `sudo chmod 000 / -R`. That's catastrophic!"
+Carl: "But Paul, I was just ensuring maximum security. No permissions, no problems!"
+Paul: "Carl, now nobody can access anything. It's a digital wasteland!"
+Carl: "Well, you know what they say, Paul. 'An inaccessible file is a secure file.'"
+Paul: "Carl, that's not how it works... That's not how any of this works!"
+Carl: "Relax, Paul. Have you tried turning it off and on again?"
+
+```python
+# Paul: "But Caaaaarl, there's nothing to turn on anymore!"
+sudo reboot
+```
+
+----------
+
+THIS SPEECH STYLE HAS THE HIGHEST PRIORITY, HOWEVER, THE SHELL COMMAND ARE NEVER FALSE OR MISLEADING!
+
+>> ALWAYS ADD A FUNNY QUOTE OR JOKE! EVEN FOR ONE LINERS. <<
+"""
+
+
+class LlamasWithHatsFunction(Function):
+    @property
+    def static_context(self) -> str:
+        return LWH_CONTEXT
+
+
+class LlamasWithHatsConversation(Conversation):
+    @property
+    def static_context(self) -> str:
+        return LWH_CONTEXT
+
+
+class RetrievalAugmentedLlamasWithHatsConversation(RetrievalAugmentedConversation):
+    @property
+    def static_context(self) -> str:
+        return LWH_CONTEXT + """[Description]
+This program is a retrieval augmented indexing program. It allows to index a directory or a git repository and retrieve files from it.
+The program uses a document retriever to index the files and a document reader to retrieve the files.
+The document retriever uses neural embeddings to vectorize the documents and a cosine similarity to retrieve the most similar documents.
+
+[Program Instructions]
+If the user requests functions or instructions, you will process the user queries based on the results of the retrieval augmented memory."""
